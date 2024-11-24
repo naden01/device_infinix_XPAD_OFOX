8eb44ccc8cd45cd72684a5106518d771acd2f531
nazephyrus <126683488+naden01@users.noreply.github.com>
2024-11-24 09:05:52 -0800

    Update Device.mk
    Signed-off-by: nazephyrus <126683488+naden01@users.noreply.github.com>

--- Device.mk
+++ Device.mk
@@ -4,6 +4,8 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+DEVICE_PATH := device/infinix/XPAD
+
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit_only.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
